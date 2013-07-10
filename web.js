9123191ffbaaa6f0cee2c8f96e1f69fce51ac43d
Xythalia <xythalia@gmail.com>
2013-07-10 22:37:59 +0000

    web.js read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+function readFile(path){
+  var data = fs.readFileSync(path)
+  console.log("Script: " + __filename + "\nRead: " + path);
+  return data;
+  
+}
+
+var string = readFile("index.html").toString();
+console.log("Read String: " + string);
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(string);
 });
 
 var port = process.env.PORT || 5000;
